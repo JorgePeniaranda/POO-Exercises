7169b79f8c7a0764788efe9c56c2ea75c59f73f4
Jorgemaxii25 <jorge.maxii25@gmail.com>
2022-11-23 01:00:35 -0300

    Ejercicio15

--- Ejercicio15/Program.cs
+++ Ejercicio15/Program.cs
@@ -26,6 +26,73 @@ namespace Ejercicio15
             agendaTelefonica.huecosLibres();
 
             Console.ReadKey();
+            
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine();
+
+                    Console.WriteLine("Lista de Opciones:");
+                    Console.WriteLine("1. Agregar Contacto");
+                    Console.WriteLine("2. Existe Contacto");
+                    Console.WriteLine("3. Mostrar Lista de Contactos");
+                    Console.WriteLine("4. Buscar Contacto");
+                    Console.WriteLine("5. Eliminar Contacto");
+                    Console.WriteLine("6. Comprobar si agenda esta llena");
+                    Console.WriteLine("7. Huecos libres");
+                    Console.WriteLine("0. Salir");
+                    Console.Write("Elige una de las opciones: ");
+                    int opcion = Convert.ToInt32(Console.ReadLine());
+
+                    switch (opcion)
+                    {
+                        case 1:
+                            agendaTelefonica.agregarContacto(userCreateContact());
+                            break;
+                        case 2:
+                            agendaTelefonica.existeContacto(userCreateContact());
+                            break;
+                        case 3:
+                            agendaTelefonica.listarContactos();
+                            break;
+                        case 4:
+                            agendaTelefonica.buscaContacto(Console.ReadLine());
+                            break;
+                        case 5:
+                            agendaTelefonica.eliminarContacto(userCreateContact());
+                            break;
+                        case 6:
+                            agendaTelefonica.agendaLlena();
+                            break;
+                        case 7:
+                            agendaTelefonica.huecosLibres();
+                            break;
+                        case 0:
+                            return;
+                        default:
+                            Console.WriteLine("Elige una opcion entre 1 y 4");
+                            break;
+                    }
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            Contacto userCreateContact()
+            {
+                Console.Write("Ingrese nombre del contacto: ");
+                string nombreConntacto = Console.ReadLine();
+
+                Console.Write("Ingrese numero del contacto: ");
+                int numeroContacto = Convert.ToInt32(Console.ReadLine());
+
+                return new Contacto(nombreConntacto, numeroContacto);
+            }
         }
     }
 }
